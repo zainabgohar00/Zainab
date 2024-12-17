3a6af24b769b7cc0a38945d38b840af98d8ce28f
zainabgohar00 <zainabgohar00@gmail.com>
2024-12-17 01:43:20 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+// create a varaible name and assign your bane to instanceof. then print : " Hello, My name is [name]"
+let name= "zainab"
+console.log("hello, my name is" ,name);
+
+// declear two numbers a=5 and b=3. calculate and print their sum,diff,product
+let a=5
+let b=3
+let sum =a+b;
+console.log("the sum of a and b" ,sum)
+let differnce =a-b;
+console.log("the difference of a and b" ,differnce)
+let product =a*b;
+console.log("the product of a and b" ,product)
+
+// write program to check if a num is even or odd
+let k = 6
+let number = k % 2 == 0 ?
+"Even Number" : "Odd Number";
+console.log(number);
+
+// write a script that checks if a number is greater than 10
+let z=19;
+let c=z> 10? "Greater" : "Smaller";
+console.log(c)
+
+// write a program to check if someone can vote or Notification. (if age is  greater than 18 than "can vote" else "cant vote")
+let y = age > 18 ?
+age=16
+"cann vote" : "cant vote"
+console.log(y)
