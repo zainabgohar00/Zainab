ee1ed8a5037aa3604a15b20a167550547283db1c
zainabgohar00 <zainabgohar00@gmail.com>
2024-12-18 01:55:21 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,80 @@
 // create a varaible name and assign your bane to instanceof. then print : " Hello, My name is [name]"
-let name= "zainab"
-console.log("hello, my name is" ,name);
+// let name= "zainab"
+// console.log("hello, my name is" ,name);
 
-// declear two numbers a=5 and b=3. calculate and print their sum,diff,product
-let a=5
-let b=3
-let sum =a+b;
-console.log("the sum of a and b" ,sum)
-let differnce =a-b;
-console.log("the difference of a and b" ,differnce)
-let product =a*b;
-console.log("the product of a and b" ,product)
+// // declear two numbers a=5 and b=3. calculate and print their sum,diff,product
+// let a=5
+// let b=3
+// let sum =a+b;
+// console.log("the sum of a and b" ,sum)
+// let differnce =a-b;
+// console.log("the difference of a and b" ,differnce)
+// let product =a*b;
+// console.log("the product of a and b" ,product)
 
-// write program to check if a num is even or odd
-let k = 6
-let number = k % 2 == 0 ?
-"Even Number" : "Odd Number";
+// // write program to check if a num is even or odd
+// let k = 6
+// let number = k % 2 == 0 ?
+// "Even Number" : "Odd Number";
+// console.log(number);
+
+// // write a script that checks if a number is greater than 10
+// let z=19;
+// let c=z> 10? "Greater" : "Smaller";
+// console.log(c)
+
+// // write a program to check if someone can vote or Notification. (if age is  greater than 18 than "can vote" else "cant vote")
+// let y = age > 18 ?
+//  age=16? 
+// "can vote" : "cant vote";
+// console.log(y)
+
+//  check if a person is eligible to drive or not using his age (age must be equal to or greater than 18)
+let y=12
+let z= y >=18?
+"you can drive" : " you cant drive ";
+console.log(z);
+ 
+// check if a number is positive or negative?
+let k=3 
+let number = k<0?
+"positive" : "negative";
 console.log(number);
 
-// write a script that checks if a number is greater than 10
-let z=19;
-let c=z> 10? "Greater" : "Smaller";
-console.log(c)
+// declear variable of diff  types and print their types
+let name = "zaynab"
+console.log(name)
+console.log("type of name is" , typeof(name))
 
-// write a program to check if someone can vote or Notification. (if age is  greater than 18 than "can vote" else "cant vote")
-let y = age > 18 ?
-age=16
-"cann vote" : "cant vote"
-console.log(y)
+let password = 34566
+console.log(password)
+console.log("type of password is" ,typeof(password))
+
+let is_pakistani = true;
+console.log(is_pakistani)
+console.log("type of is pakistani is" ,typeof(is_pakistani))
+
+// for log
+// console.log(a,accountName,accountPassword,phoneNumber,city)
+
+// for table 
+//  console.table([accountID,accountName,accountPassword,phoneNumber,city])
+
+// write a script that calculates the total cost of items (cost1=67, cost2= 456, cost3= 566, cost4= 567)
+let cost1 = 67
+let cost2 = 456
+let cost3 = 566 
+let cost4 = 567
+let sum= cost1+cost2+cost3+cost4
+console.log("total cost of items " ,sum);
+
+// write a script to calculate the perimeter of rectangle (perimerter of rectangle = 2*lenght + 2*breadth )
+let lenght= 2.3
+let breadth= 4.6
+let perimerter_of_rectangle = (2*lenght) + (2*breadth)
+console.log("perimeter of rectangle" , perimerter_of_rectangle+"cm")
+
+// write a program that finds the position of first space in the string
+let str= "we are learing java script"
+console.log("the position of java is", str.indexOf(" "))
+
